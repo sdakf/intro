0a954918356d42441efbc6e6c04f1b47bea8ba6d
kf <falinskikrzysztof@gmail.com>
2018-07-02 19:16:26 +0200

    text

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,40 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class App {
+
+    private static List<String> animals = Arrays.asList("cat", "dog", "mouse", "  rat", " pig ", "rabbit ", "hamster", "parrot"); // Tradycyjna pętla
+
     public static void main(String[] args) {
-        System.out.println(args[0]);
+        simplePrintFirstElementFromInput(args[0]);
+        displayTextFromInputWithCommas(animals);
+        displayTextFromInputWithCommasAndWithoutWhiteSpacesAndWithUppercase(animals);
+    }
+
+    private static void displayTextFromInputWithCommasAndWithoutWhiteSpacesAndWithUppercase(List<String> collection) {
+        int i = 1;
+        for (String element : collection) {
+            System.out.print(element.toUpperCase().trim() + (i > element.length() ? "" : ","));
+            i++;
+        }
+        System.out.println();
+        System.out.println(collection.stream().map(e->e.toUpperCase().trim()).collect(Collectors.joining(",")));
+    }
+
+    private static void displayTextFromInputWithCommas(Collection<String> collection) {
+        int i = 1;
+        for (String element : collection) {
+            System.out.print(element + (i > element.length() ? "" : ","));
+            i++;
+        }
+        System.out.println();
+        System.out.println(String.join(",", collection));
+    }
+
+
+    private static void simplePrintFirstElementFromInput(String arg) {
+        System.out.println(arg);
     }
 }
