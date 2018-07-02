5c5867b34f37e186b0b16501aeec73665f4730f0
kf <falinskikrzysztof@gmail.com>
2018-07-02 21:06:30 +0200

    simple web calculator with history in file

--- src/main/java/simpleapp/CalculatorService.java
+++ src/main/java/simpleapp/CalculatorService.java
@@ -4,6 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -14,9 +22,35 @@ public class CalculatorService {
     public String calculate(@RequestParam(required = false) String input, Map<String, Object> model) {
         if (input != null && !input.isEmpty()) {
             String[] split = input.split(" ");
-            model.put("result", "Twój wynik to: " + calculate(Integer.valueOf(split[0]), Integer.valueOf(split[2]), split[1]));
+            String result = calculate(Integer.valueOf(split[0]), Integer.valueOf(split[2]), split[1]);
+            model.put("result", "Twój wynik to: " + result);
+            saveResultToFile(split[0] + split[1] + split[2] + "=" + result);
+            List<String> history = getHistory();
+            model.put("history", history);
+
         }
         return "calcPage";
+    }
+
+    private List<String> getHistory() {
+        try {
+            return Files.readAllLines(Paths.get(this.getClass().getClassLoader().getResource("historia.txt").toURI()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+    private void saveResultToFile(String result) {
+        try {
+            Files.write(Paths.get(this.getClass().getClassLoader().getResource("historia.txt").toURI()), Arrays.asList(result), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 
 
